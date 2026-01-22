59fd4a4314fbfa8feeb62bb3a97835e65789000f
Egor <egorbla228@gmail.com>
2026-01-22 14:19:49 +0300

    Initial Commit

---
+++ Lesson_8_Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_8_Homework
+//
+//  Created by Egor on 22.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
